9162480cec2cb772710f45dd5aa79a8991bb1044
gulyaeve <baddman@list.ru>
2023-08-15 12:44:59 +0300

    added webcam capture

--- app.py
+++ app.py
@@ -11,5 +11,27 @@ def main():
         print("exit")
 
 
+def webcam_capture():
+    capture = cv2.VideoCapture(0)
+
+    if not capture.isOpened():
+        print("Камера не может быть открыта.")
+        exit()
+
+    while True:
+        ret, frame = capture.read()
+        if not ret:
+            print("Камера не может быть открыта.")
+            break
+        cv2.imshow("WEBCAM", frame)
+
+        k = cv2.waitKey(0)
+        if k == ord("s"):
+            print("exit")
+            break
+    capture.release()
+    cv2.destroyAllWindows()
+
+
 if __name__ == '__main__':
-    main()
+    webcam_capture()
